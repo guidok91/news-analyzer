b358dc399d9b2086aaf9a27858266fa3c4856048
GK <guidokosloff@gmail.com>
2022-01-28 14:10:44 -0300

    Rearrange packages

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,6 +1,6 @@
+import calculator.Calculator
+
 @main
 def main(x: Int, others: String*): Unit = {
-  println(s"The double of $x is ${double(x)}")
+  println(s"The double of $x is ${Calculator.double(x)}")
 }
-
-def double(x: Int): Int = x * 2
---
+++ src/main/scala/calculator/Calculator.scala
@@ -0,0 +1,5 @@
+package calculator
+
+object Calculator {
+  def double(x: Int): Int = x * 2
+}
