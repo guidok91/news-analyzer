6f9051a88fd5cc7b8c91f91012bb82df8ec8afb4
Guido Kosloff Gancedo <gkg@hellofresh.com>
2022-02-01 11:32:40 -0300

    Print sample tweets

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -2,8 +2,25 @@ import config.ConfigManager
 
 @main
 def main(args: String*): Unit = {
-  val r = requests.get("https://api.github.com/users/lihaoyi")
-  val json = ujson.read(r.text()).obj
-  println(s"Response json keys: ${json.keys}")
-  println(s"Response json login: ${json.obj.get("login")}")
+  val config = ConfigManager.config
+
+  val bearerToken = config.getString("twitter_api.auth_bearer_token")
+  val response = requests.get(
+    url = "https://api.twitter.com/2/tweets/search/recent",
+    params = Map(
+      "query" -> "from:TwitterDev",
+      "tweet.fields" -> "created_at",
+      "expansions" -> "author_id",
+      "user.fields" -> "created_at"
+    ),
+    headers = Map("Authorization" -> s"Bearer $bearerToken")
+  )
+  val response_data = ujson.read(response.text())("data").arr
+
+  response_data.foreach(
+    tweet => println(
+      s"Tweet author ID: ${tweet("author_id")}\n" +
+      s"Tweet text: ${tweet("text")}\n"
+    )
+  )
 }
