f02916c51987eea25dcf83f465d34bef3ecb20ae
Guido Kosloff Gancedo <gkg@hellofresh.com>
2022-02-03 08:09:09 -0300

    Add test for extractTweets non-empty

--- src/test/scala/TwitterAPIClientTest.scala
+++ src/test/scala/TwitterAPIClientTest.scala
@@ -4,15 +4,26 @@ class TwitterAPIClientTest extends AnyFunSuite {
   val twitterApiClient = new TwitterAPIClient("test token")
 
   test("Test buildSearchQuery with non-empty input") {
-    val tweetKeywords = List("hashtag", "tweet")
-    assert(twitterApiClient.buildSearchQuery(tweetKeywords) == "hashtag OR tweet OR #hashtag OR #tweet")
+    val inputTweetKeywords = List("hashtag", "tweet")
+    val outputExpected = "hashtag OR tweet OR #hashtag OR #tweet"
+    val output = twitterApiClient.buildSearchQuery(inputTweetKeywords)
+    assert(outputExpected == output)
   }
 
   test("Test buildSearchQuery with empty input") {
-    val tweetKeywords = List()
+    val inputTweetKeywords = List()
     assertThrows[NoDataFoundException] {
-      twitterApiClient.buildSearchQuery(tweetKeywords)
+      twitterApiClient.buildSearchQuery(inputTweetKeywords)
     }
+  }
+
+  test("Test extractTweets with non-empty input") {
+    val inputResponse = """{
+      "data": [{"id": "123", "text": "tweet1"}, {"id": "456", "text": "tweet2"}]
+    }"""
+    val outputExpected = List(Map("id" -> "123", "text" -> "tweet1"), Map("id" -> "456", "text" -> "tweet2"))
+    val output = twitterApiClient.extractTweets(inputResponse)
+    assert(outputExpected == output)
   }
 
   // TODO test extractTweets and getTweets (mocking requests.get)
