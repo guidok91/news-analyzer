acc1b91a2bf94bc7cfe57073c685cbb3bc6120a2
Guido Kosloff Gancedo <gkg@hellofresh.com>
2022-02-03 07:58:52 -0300

    Fix uppercase

--- src/main/scala/TwitterAPIClient.scala
+++ src/main/scala/TwitterAPIClient.scala
@@ -20,7 +20,7 @@ class TwitterAPIClient(bearerToken: String) {
   }
 
   def buildSearchQuery(tweetKeywords: List[String]): String = {
-    if tweetKeywords.isEmpty then throw NoDataFoundException("At least one Tweet search keyword must be specified")
+    if tweetKeywords.isEmpty then throw NoDataFoundException("At least one tweet search keyword must be specified")
     (tweetKeywords ++ tweetKeywords.map(t => s"#$t")).mkString(" OR ")
   }
 
