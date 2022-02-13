50f91c44d12c7e4687ea4e9681cf8e95fac7287b
GK <guidokosloff@gmail.com>
2022-02-13 17:50:28 -0300

    Add tests for Sentiment

---
+++ src/test/scala/SentimentTest.scala
@@ -0,0 +1,74 @@
+import org.scalatest.funsuite.AnyFunSuite
+import java.util.zip.ZipError
+
+class SentimentTest extends AnyFunSuite {
+  test("Test toSentiment with sentiment = 0") {
+    // GIVEN
+    val input = 0
+    val outputExpected = Sentiment.NEGATIVE
+
+    // WHEN
+    val output = Sentiment.toSentiment(input)
+
+    // THEN
+    assert(outputExpected == output)
+  }
+
+  test("Test toSentiment with sentiment = 1") {
+    // GIVEN
+    val input = 1
+    val outputExpected = Sentiment.NEGATIVE
+
+    // WHEN
+    val output = Sentiment.toSentiment(input)
+
+    // THEN
+    assert(outputExpected == output)
+  }
+
+  test("Test toSentiment with sentiment = 2") {
+    // GIVEN
+    val input = 2
+    val outputExpected = Sentiment.NEUTRAL
+
+    // WHEN
+    val output = Sentiment.toSentiment(input)
+
+    // THEN
+    assert(outputExpected == output)
+  }
+
+  test("Test toSentiment with sentiment = 3") {
+    // GIVEN
+    val input = 3
+    val outputExpected = Sentiment.POSITIVE
+
+    // WHEN
+    val output = Sentiment.toSentiment(input)
+
+    // THEN
+    assert(outputExpected == output)
+  }
+
+  test("Test toSentiment with sentiment = 4") {
+    // GIVEN
+    val input = 4
+    val outputExpected = Sentiment.POSITIVE
+
+    // WHEN
+    val output = Sentiment.toSentiment(input)
+
+    // THEN
+    assert(outputExpected == output)
+  }
+
+  test("Test toSentiment with invalid sentiment") {
+    // GIVEN
+    val input = 5
+
+    // THEN
+    assertThrows[MatchError] {
+      Sentiment.toSentiment(input)
+    }
+  }
+}
