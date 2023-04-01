6fe3890ecf21cdece2cabccd5e788eec9f47afd1
GK <guidokosloff@gmail.com>
2023-04-01 09:18:33 +0200

    Rename tweet arg

--- src/main/scala/tweeter/Tweet.scala
+++ src/main/scala/tweeter/Tweet.scala
@@ -3,12 +3,12 @@ package tweeter
 import sentiment.{Sentiment, SentimentAnalyzer}
 import java.time.Instant
 
-class Tweet(tweet_map: Map[String, Any]) {
-  val id = tweet_map("id").asInstanceOf[String].toLong
-  val author_id = tweet_map("author_id").asInstanceOf[String].toLong
-  val text = tweet_map("text").asInstanceOf[String]
+class Tweet(tweet: Map[String, Any]) {
+  val id = tweet("id").asInstanceOf[String].toLong
+  val author_id = tweet("author_id").asInstanceOf[String].toLong
+  val text = tweet("text").asInstanceOf[String]
   val created_at =
-    Instant.parse(tweet_map("created_at").asInstanceOf[String]).getEpochSecond
-  val lang = tweet_map("lang").asInstanceOf[String]
+    Instant.parse(tweet("created_at").asInstanceOf[String]).getEpochSecond
+  val lang = tweet("lang").asInstanceOf[String]
   val sentiment = SentimentAnalyzer.getSentiment(text)
 }
